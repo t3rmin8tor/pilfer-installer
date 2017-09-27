45a3a6b2bc705abd49d9b651425aeef158761941
t3rmin8tor <mark.barker2@sky.com>
2017-09-27 19:00:53 +0100

    Add files via upload

---
+++ pilfer/pilfer/record.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3 
+
+import shlex
+import sys, os
+import subprocess
+from datetime import datetime 
+
+def ffmpeg(**kwargs):
+    ''' ffmpeg function
+
+    ffmpeg recording function
+    '''
+    home = os.path.expanduser('~')
+    desktop = 'Desktop'
+    time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    ext = 'mkv'
+    recordingfile = os.path.join(home, desktop, 'video-{}.{}'.format(time, ext))
+    
+    if sys.platform.startswith('linux'):
+        ffmpeg = 'ffmpeg'
+    elif sys.platform.startswith('freebsd'):
+        ffmpeg = 'ffmpeg'
+    elif sys.platform.startswith('win32'):
+        home = os.path.expanduser('~').replace('\\', '/')
+        desktop = '//Desktop//'
+        time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        ext = '.mkv'
+        recordingfile = home + desktop + 'video-' + time + ext
+        ffmpeg = 'C:/pilfer/system/bin/ffmpeg'
+    elif sys.platform.startswith('darwin'):
+        bin = 'bin'
+        ffmpeg = os.path.join(home, bin, 'ffmpeg')
+
+    # get the values from the dictionary passed to the function
+    values = list(kwargs.values())
+    # url from kwargs which is the dictionary passed to the function
+    url = kwargs['url']
+
+    ffcmd = "{0} -hide_banner -stats -v panic -i {1} -c:v copy -c:a copy {2}".format(ffmpeg, url, recordingfile)
+
+    if any(word in kwargs for word in ('user-agent', 'referer', 'cookie')):                 
+        # dict minus first time which is the url
+        options = values[1:]
+        remove_bracket = str(options)[1:-1]
+        options_join = ''.join(remove_bracket).replace('"', '')
+        ffcmd = "{0} -hide_banner -stats -v panic {2} -i {1} -c:v copy -c:a copy {3}".format(ffmpeg, url, options_join, recordingfile)
+
+    if 'duration' in kwargs:
+        tflag = kwargs['tflag']
+        duration = kwargs['duration']
+        # dict minus first time which is the url, and minus the last 2 items tflag and duration
+        options = values[1:-2]
+        remove_bracket = str(options)[1:-1]
+        options_join = ''.join(remove_bracket).replace('"', '')
+        ffcmd = "{0} -hide_banner -stats -v panic {2} -i {1} -c:v copy -c:a copy {3} {4} {5}".format(ffmpeg, url, options_join, tflag, duration, recordingfile)
+
+
+    # split the ffmpeg command for subprocess
+    ffsplit = shlex.split(ffcmd)
+
+    print("running ffmpeg command:")
+ 
+    # try ffmpeg function except keyboard interupt if user quits script with control c
+    try:
+        process = subprocess.run(ffsplit)
+    except KeyboardInterrupt:
+        print("recording stopped by user")
+    except IOError:
+        print("input outpur error, is ffmpeg installed")
+
+
+def rtmp(**kwargs):
+    ''' rtmpdump function
+
+    rtmpdump recording function
+    '''
+    home = os.path.expanduser('~')
+    desktop = 'Desktop'
+    time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    ext = 'mkv'
+    recordingfile = os.path.join(home, desktop, 'video-{}.{}'.format(time, ext))
+    
+    if sys.platform.startswith('linux'):
+        ffmpeg = 'ffmpeg'
+        rtmpdump = 'rtmpdump'
+    elif sys.platform.startswith('freebsd'):
+        ffmpeg = 'ffmpeg'
+        rtmpdump = 'rtmpdump'
+    elif sys.platform.startswith('win32'):
+        home = os.path.expanduser('~').replace('\\', '/')
+        desktop = '//Desktop//'
+        time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        ext = '.mkv'
+        recordingfile = home + desktop + 'video-' + time + ext
+        ffmpeg = 'C:/pilfer/system/bin/ffmpeg'
+        rtmpdump = 'C:/pilfer/system/bin/rtmpdump'
+    elif sys.platform.startswith('darwin'):
+        bin = 'bin'
+        ffmpeg = os.path.join(home, bin, 'ffmpeg')
+        rtmpdump = '/usr/local/bin/rtmpdump'
+
+    # url from kwargs which is the dictionary passed to the function
+    url = kwargs['url']
+
+    rtmpcmd = "{0} -i '{1}' -o {2}".format(rtmpdump, url, recordingfile)
+
+    if 'duration' in kwargs:
+        tflag = kwargs['tflag']
+        duration = kwargs['duration']
+        rtmpcmd = "{0} -q -i '{1}' | {2} -hide_banner -stats -v panic -i - -c:v copy -c:a copy {3} {4} {5}".format(rtmpdump, url, ffmpeg, tflag, duration, recordingfile)
+
+    print("running rtmp command:")
+ 
+    # try ffmpeg function except keyboard interupt if user quits script with control c
+    try:
+        process = subprocess.run(rtmpcmd, shell=True)
+    except KeyboardInterrupt:
+        print("recording stopped by user")
+    except IOError:
+        print("input outpur error, is rtmpdump or ffmpeg installed")
---
+++ pilfer/pilfer/validate.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3 
+
+import sys, re, getopt, os.path, mimetypes
+
+def usage():
+    '''script usage
+    
+    display script usage when the -h or --help option is used
+    '''
+    usage = """
+-h --help display help
+-i 'video url' or text file
+-t '00:00:00'
+
+pilfer -i <video-url> -t <00:00:00>
+    """
+    print(usage)
+
+def usageplay():
+    '''script usage
+    
+    display script usage when the -h or --help option is used
+    '''
+    usageplay = """
+-h --help display help
+-i 'video url' or text file
+
+pilfer-play -i <video-url>
+    """
+    print(usageplay)
+
+def checkurl(url):
+    ''' check first argument passed to script
+    
+    check if first argument passed to script, 
+    is a text file and if so open it and read the contents,
+    and set the variable url to equal the contents of the text file.
+    if the first argument isnt a text file its on the command line
+    
+    then check if the string is a url
+    '''
+    if os.path.isfile(url):
+        if mimetypes.guess_type(url)[0] == 'text/plain':
+            with open(url, 'r') as file:
+                url = file.read()
+            file.close()          
+            return url
+
+    x = re.compile(r'^(http|https|rtmp|rtmpe)://.*$')
+    if not x.match(url):
+        usage()    # display script usage
+        sys.exit() # exit
+    return url # return url needed to run script from kodi
+    
+def durationValidated(duration):
+    ''' validate user input for second argument
+
+    user regular expressions to check duration matches 00:00:00 format
+    '''
+    y = re.compile('^[0-9]{2}:[0-9]{2}:[0-9]{2}$')
+    if not y.match(duration):
+        usage()    # display script usage
+        sys.exit() # exit
+    return duration
